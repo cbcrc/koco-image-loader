f3ea70d0045401a841e71d6bee6a7d4ea8f02122
malikov <vincent.maliko@cbc.ca>
2015-06-03 14:57:55 -0400

    add copyright lines

--- src/image-loader-ui.js
+++ src/image-loader-ui.js
@@ -1,3 +1,6 @@
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 define(['text!./image-loader.html', 'jquery', 'knockout'],
     function(template, $, ko) {
         'use strict';
